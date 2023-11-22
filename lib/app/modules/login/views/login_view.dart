5b9f41dbe7a9a8e354aea013332b668b669602c9
Simple-Genius <ajila.adeboye@gmail.com>
2023-11-22 06:23:58 +0100

    login view in progress

--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -9,16 +9,36 @@ class LoginView extends GetView<LoginController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('LoginView'),
-        centerTitle: true,
+        body: Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            height: 100,
+          ),
+          Text(
+            'Welcome\nBack',
+            style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 10),
+          Text(
+            'Sign in to continue',
+            style: TextStyle(color: const Color.fromARGB(255, 146, 141, 141)),
+          ),
+          SizedBox(height: 30),
+          TextFormField(
+            cursorColor: Color.fromARGB(255, 143, 138, 138),
+            decoration: InputDecoration(
+                contentPadding: EdgeInsets.all(15),
+                filled: true,
+                fillColor: Color.fromARGB(255, 187, 184, 201),
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide.none)),
+          )
+        ],
       ),
-      body: const Center(
-        child: Text(
-          'LoginView is working',
-          style: TextStyle(fontSize: 20),
-        ),
-      ),
-    );
+    ));
   }
 }

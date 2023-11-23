63e0b5495f8583fe201fe83601c526f98354595d
Simple-Genius <ajila.adeboye@gmail.com>
2023-11-23 07:58:31 +0100

    log in view UI complete

--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -1,8 +1,8 @@
+import 'package:chat_app/app/modules/login/controllers/login_controller.dart';
+import 'package:chat_app/app/modules/signup/views/signup_view.dart';
+import 'package:chat_app/app/modules/widgets/details_field.dart';
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
-
-import '../controllers/login_controller.dart';
 
 class LoginView extends GetView<LoginController> {
   const LoginView({Key? key}) : super(key: key);
@@ -15,7 +15,7 @@ class LoginView extends GetView<LoginController> {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           SizedBox(
-            height: 100,
+            height: 150,
           ),
           Text(
             'Welcome\nBack',
@@ -27,16 +27,61 @@ class LoginView extends GetView<LoginController> {
             style: TextStyle(color: const Color.fromARGB(255, 146, 141, 141)),
           ),
           SizedBox(height: 30),
-          TextFormField(
-            cursorColor: Color.fromARGB(255, 143, 138, 138),
-            decoration: InputDecoration(
-                contentPadding: EdgeInsets.all(15),
-                filled: true,
-                fillColor: Color.fromARGB(255, 187, 184, 201),
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide.none)),
-          )
+          DetailsFeild(),
+          SizedBox(height: 10),
+          DetailsFeild(),
+          SizedBox(height: 40),
+          Align(
+              child: Text(
+            'Forgot Password?',
+            style: TextStyle(fontWeight: FontWeight.w500, fontSize: 15),
+          )),
+          SizedBox(height: 30),
+          Align(
+            child: OutlinedButton(
+              style: ButtonStyle(
+                  padding: MaterialStateProperty.all(
+                      EdgeInsets.symmetric(vertical: 15, horizontal: 90)),
+                  shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10))),
+                  side: MaterialStateProperty.all(BorderSide(
+                      color: Colors.blue,
+                      width: 1.0,
+                      style: BorderStyle.solid))),
+              onPressed: () {},
+              child: Text(
+                'Sign in with Facebook',
+                style: TextStyle(
+                    color: Color.fromARGB(255, 17, 120, 210), fontSize: 17),
+              ),
+            ),
+          ),
+          SizedBox(height: 100),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Create new account?'),
+              TextButton(
+                  onPressed: () {
+                    Get.to(() => SignupView());
+                  },
+                  child: Text(
+                    'Sign Up',
+                    style: TextStyle(color: Colors.blue),
+                  ))
+            ],
+          ),
+          SizedBox(height: 10),
+          Align(
+              child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    padding:
+                        EdgeInsets.symmetric(vertical: 16, horizontal: 160),
+                  ),
+                  child: Text('Login')))
         ],
       ),
     ));

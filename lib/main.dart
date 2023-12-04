895ecbb8a05bf3c73b4a068f1031f36478a863a8
Simple-Genius <ajila.adeboye@gmail.com>
2023-12-04 07:02:10 +0100

    added firebase to the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
 import 'package:chat_app/app/modules/signup/controllers/signup_controller.dart';
+import 'package:chat_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 
 import 'app/routes/app_pages.dart';
 
-void main() {
+Future<void> main() async {
   Get.put(SignupController());
+  WidgetsFlutterBinding
+      .ensureInitialized(); //tells Flutter not to start running the application widget code until the Flutter framework is completely booted. Firebase uses native platform channels, which require the framework to be running.
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  ); //sets up a connection between your Flutter app and your Firebase project. The DefaultFirebaseOptions.currentPlatform is imported from our generated firebase_options.dart file. This static value detects which platform you're running on, and passes in the corresponding Firebase keys.
   runApp(
     GetMaterialApp(
       title: "Application",

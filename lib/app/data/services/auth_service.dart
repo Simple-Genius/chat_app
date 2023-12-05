a933dbfa1d4d9625befd8ee3dd526b405ecd1719
Simple-Genius <ajila.adeboye@gmail.com>
2023-12-05 08:23:24 +0100

    sign up and sign in functions

---
+++ lib/app/data/services/auth_service.dart
@@ -0,0 +1,32 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get/get.dart';
+
+class AuthService extends GetxService {
+  FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Rxn<User?> user = Rxn<User?>();
+
+  Future<User?> signUpWithEmailAndPassword(
+      String email, String password) async {
+    try {
+      UserCredential userCredential = await _auth
+          .createUserWithEmailAndPassword(email: email, password: password);
+      return userCredential.user;
+    } catch (e) {
+      print(e.toString());
+    }
+    return null;
+  }
+
+  Future<User?> signInWithEmailAndPassword(
+      String email, String password) async {
+    try {
+      UserCredential userCredential = await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      return userCredential.user;
+    } catch (e) {
+      print(e.toString());
+    }
+    return null;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/app/data/services/auth_service.dart';
 import 'package:chat_app/app/modules/signup/controllers/signup_controller.dart';
 import 'package:chat_app/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -9,16 +10,20 @@ import 'app/routes/app_pages.dart';
 
 Future<void> main() async {
   Get.put(SignupController());
+
   WidgetsFlutterBinding
       .ensureInitialized(); //tells Flutter not to start running the application widget code until the Flutter framework is completely booted. Firebase uses native platform channels, which require the framework to be running.
 
+//sets up a connection between your Flutter app and your Firebase project. The DefaultFirebaseOptions.currentPlatform is imported from our generated firebase_options.dart file. This static value detects which platform you're running on,
+//and passes in the corresponding Firebase keys.
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
-  ); //sets up a connection between your Flutter app and your Firebase project. The DefaultFirebaseOptions.currentPlatform is imported from our generated firebase_options.dart file. This static value detects which platform you're running on, and passes in the corresponding Firebase keys.
+  );
+  await Get.putAsync<AuthService>(() async => AuthService());
   runApp(
     GetMaterialApp(
       title: "Application",
-      initialRoute: Routes.DASHBOARD,
+      initialRoute: Routes.SIGNUP,
       getPages: AppPages.routes,
     ),
   );
